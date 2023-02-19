0ea998d1b25d276cf673c3a84ac4cdc6623667aa
Sergey Maslov <sergey.s.maslov@gmail.com>
2023-02-19 17:13:04 +0000

    Add some classes and styles

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-  return "<p>Hello, World</p>"
+def hello_there():
+  return render_template("home.html")
 
 
 @app.route("/test/")
