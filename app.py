fce768f06005eb2b396ddc81291ba4bc0934839d
Sergey Maslov <sergey.s.maslov@gmail.com>
2023-02-22 17:51:44 +0000

    Add work with templates

--- app.py
+++ app.py
@@ -2,14 +2,36 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS=[
+  {
+    'id': 1,
+    'title': "Data Analyst",
+    'location': 'Moscow',
+    'salary': 'Rub: 180000'
+  },
+  {
+    'id': 2,
+    'title': "Junior Data Analyst",
+    'location': 'Moscow',
+    'salary': 'Rub: 120000'
+  },
+  {
+    'id': 3,
+    'title': "Java Developer",
+    'location': 'Moscow',
+    'salary': 'Rub: 280000'
+  },
+  {
+    'id': 4,
+    'title': "Senior Data Analyst",
+    'location': 'Moscow',
+    'salary': 'Rub: 280000'
+  }
+]
+
 @app.route("/")
 def hello_there():
-  return render_template("home.html")
-
-
-@app.route("/test/")
-def hello_world2():
-  return "<p>Hello, World 2</p>"
+  return render_template("home.html", jobs=JOBS)
 
 
 if __name__ == '__main__':
