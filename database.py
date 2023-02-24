e0be944bb7e1be3566006f8e6748769065a6072b
Sergey Maslov <sergey.s.maslov@gmail.com>
2023-02-24 20:26:31 +0000

    Сохраняем отклики в базе

--- database.py
+++ database.py
@@ -28,4 +28,19 @@ def load_job_from_db(id):
     else:
       return dict(rows[0]._mapping)
     
-  
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as con:
+    query = text("insert into applications(job_id, full_name, email, linkedin_url, education, work_experience, resume_url) values (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+    con.execute(
+                  query,
+                  {
+                    'job_id': job_id, 
+                    'full_name': data['full_name'],
+                    'email': data['email'],
+                    'linkedin_url': data['linkedin_url'],
+                    'education': data['education'],
+                    'work_experience': data['work_experience'],
+                    'resume_url': data['resume_url']
+                  }
+               )
