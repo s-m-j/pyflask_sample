31b16b5507331da2e00d0c0ed985208f5a9cd77b
Sergey Maslov <sergey.s.maslov@gmail.com>
2023-02-25 06:54:47 +0000

    Добавлены API /api/jobs и /api/job/<id>

--- app.py
+++ app.py
@@ -18,6 +18,12 @@ def show_job(id):
   return render_template('jobpage.html', job=job)
 
 
+@app.route("/api/job/<id>") 
+def show_job_json(id):
+  job = load_job_from_db(id)
+  return jsonify(job)
+
+
 @app.route("/job/<id>/apply", methods=['post']) 
 def apply_to_job(id):
   data = request.form  
